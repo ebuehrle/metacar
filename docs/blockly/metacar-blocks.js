219672be3e0b98016ebf543f9c9240428202958e
Etienne Bührle <43623224+ebuehrle@users.noreply.github.com>
2021-02-25 22:21:39 +0100

    Complete German Locale

--- docs/blockly/metacar-blocks.js
+++ docs/blockly/metacar-blocks.js
@@ -1,11 +1,11 @@
 Blockly.Blocks['forward'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("forward");
+        .appendField("vorwärts");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("Move forward.");
+ this.setTooltip("Vorwärts fahren.");
  this.setHelpUrl("");
   }
 };
@@ -13,11 +13,11 @@ Blockly.Blocks['forward'] = {
 Blockly.Blocks['turn'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["turn left","left"], ["turn right","right"]]), "turn_direction");
+        .appendField(new Blockly.FieldDropdown([["nach links abbiegen","left"], ["nach rechts abbiegen","right"]]), "turn_direction");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("");
+ this.setTooltip("Auf der Stelle drehen.");
  this.setHelpUrl("");
   }
 };
@@ -25,11 +25,11 @@ Blockly.Blocks['turn'] = {
 Blockly.Blocks['backward'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("backward");
+        .appendField("rückwärts");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("Move backward.");
+ this.setTooltip("Rückwärts fahren.");
  this.setHelpUrl("");
   }
 };
@@ -41,7 +41,7 @@ Blockly.Blocks['stop'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("Brake to zero speed.");
+ this.setTooltip("Stehen bleiben.");
  this.setHelpUrl("");
   }
 };
@@ -49,13 +49,13 @@ Blockly.Blocks['stop'] = {
 Blockly.Blocks['sleep'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("sleep for")
+        .appendField("warte")
         .appendField(new Blockly.FieldNumber(500, 0, Infinity, 1), "millis")
-        .appendField("milliseconds");
+        .appendField("Millisekunden");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("Don't do this when you're driving.");
+ this.setTooltip("Eine gewisse Zeit abwarten und weiterfahren.");
  this.setHelpUrl("");
   }
 };
@@ -63,12 +63,12 @@ Blockly.Blocks['sleep'] = {
 Blockly.Blocks['lidar'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("there is")
-        .appendField(new Blockly.FieldDropdown([["a car","car"], ["a road","road"], ["terrain","terrain"]]), "obstacle_type")
+        .appendField("da ist")
+        .appendField(new Blockly.FieldDropdown([["ein Auto","car"], ["eine Straße","road"], ["etwas anderes","terrain"]]), "obstacle_type")
         .appendField(new Blockly.FieldDropdown([["0m","0m"], ["1m","1m"], ["2m","2m"], ["3m","3m"], ["4m","4m"]]), "front_distance")
-        .appendField("in front,")
+        .appendField("vor mir,")
         .appendField(new Blockly.FieldDropdown([["0m","0m"], ["1m","1m"], ["2m","2m"]]), "lateral_distance")
-        .appendField(new Blockly.FieldDropdown([["to the left","left"], ["to the right","right"]]), "lateral_direction");
+        .appendField(new Blockly.FieldDropdown([["links von mir","left"], ["rechts von mir","right"]]), "lateral_direction");
     this.setOutput(true, "Boolean");
     this.setColour(230);
  this.setTooltip("");
